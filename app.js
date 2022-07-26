998d64d7d252c96e6667b11546af123ee9f61401
Lalit Ghimire <lalitghimire@gmail.com>
2022-07-26 22:38:54 +0300

    fix for lint error

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const app = express();
-
+require('dotenv').config()
+const express = require('express')
+const app = express()
 // Heroku dynamically sets a port
-const PORT = process.env.PORT || 5000;
-
-app.use(express.static("dist"));
+const PORT = process.env.PORT || 5000
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log("server started on port 5000");
-});
+  /* eslint-disable */ console.log('server started on port 5000')
+})
