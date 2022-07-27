8721b12142169faaf09acfef7c817261979619ae
Lalit Ghimire <lalitghimire@gmail.com>
2022-07-27 17:56:29 +0300

    add e2e test with cypress

---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    /* eslint-disable */ setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
