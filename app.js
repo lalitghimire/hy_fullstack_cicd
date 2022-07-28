56d4f2190f07bbba356caf0198b1ac97d3ea8766
Lalit Ghimire <lalitghimire@gmail.com>
2022-07-28 16:35:59 +0300

    add health check steps

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = express()
 const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
+// health check for deployment
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+app.get('/version', (req, res) => {
+  res.send('2.1') // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   /* eslint-disable */ console.log('server started on port 5000')
 })
